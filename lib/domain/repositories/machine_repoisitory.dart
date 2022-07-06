a8279ce23ac089ff16f7204dacd8afcdddf93705
nguyen y nhi <nhi.nguyeny.sistrain@gmail.com>
2022-07-06 16:04:06 +0700

    initail respository

---
+++ lib/domain/repositories/machine_repoisitory.dart
@@ -0,0 +1,10 @@
+
+import 'dart:ffi';
+
+import 'package:injection_molding_machine_application/domain/entities/configuration.dart';
+import 'package:injection_molding_machine_application/domain/entities/preShift.dart';
+
+abstract class MachineRepository {
+  Future<List<Product>> getProductDetail();
+  Future<List<PreShift>> getPreShiftData();
+}
